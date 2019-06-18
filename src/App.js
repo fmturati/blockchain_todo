8fec74cbad21f3192678f2d3ad299a5058470fd2
FhelipeTurati <fmturati@gmail.com>
2019-06-18 10:27:04 -0700

    setting the web3js and metamask

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Web3 from "web3";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// early sunny awake mixed sign sure window bomb meadow buffalo exclude avocado
+
+class App extends Component {
+  componentWillMount() {
+    this.loadBlockchainData();
+  }
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const network = await web3.eth.net.getNetworkType();
+    console.log("network:", network);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Blockchain Todo</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
