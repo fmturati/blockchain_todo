d4a48ebdaeb53635235d9b2fd6057bced92db7b7
FhelipeTurati <fmturati@gmail.com>
2019-06-18 10:55:43 -0700

    getting access to blockchain account

--- src/App.js
+++ src/App.js
@@ -10,15 +10,26 @@ class App extends Component {
   }
 
   async loadBlockchainData() {
-    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
-    const network = await web3.eth.net.getNetworkType();
-    console.log("network:", network);
+    const web3 = new Web3(Web3.givenProvider || "http://127.0.0.1:8545");
+    // const network = await web3.eth.net.getNetworkType();
+    // needed to enable the connection
+    await window.ethereum.enable();
+    // get accounts
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
+    console.log("accounts", accounts[0]);
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { account: "" };
   }
 
   render() {
     return (
       <div className="container">
         <h1>Blockchain Todo</h1>
+        <p>Your account: {this.state.account}</p>
       </div>
     );
   }
